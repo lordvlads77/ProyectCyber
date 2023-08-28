6f94ea068bbb04df2869c9c978bc175456210e0f
Pollogamerz <tbarajasglez05@gmail.com>
2023-08-28 16:08:10 -0600

    Movimiento

---
+++ Assets/Scenes/Movimiento.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float runSpeedMultiplier = 2f;
+
+    private CharacterController controller;
+    private bool isRunning = false;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+        // Cambiar entre correr y caminar
+        isRunning = Input.GetKey(KeyCode.LeftShift);
+
+        // Aplicar velocidad de movimiento
+        float currentSpeed = isRunning ? moveSpeed * runSpeedMultiplier : moveSpeed;
+        controller.Move(movement * currentSpeed * Time.deltaTime);
+    }
+}
