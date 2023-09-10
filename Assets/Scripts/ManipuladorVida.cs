6eae05a3e708c846e4948fd16eba931a6e4a055f
Pollogamerz <tbarajasglez05@gmail.com>
2023-09-09 22:16:51 -0600

    vidanofuncional

---
+++ Assets/Scripts/ManipuladorVida.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManipuladorVida : MonoBehaviour
+{
+    VidaPlayer playervida;
+    public int cantidad;
+    public float damageTime;
+    float currentDamageTime;
+
+    void Start()
+    {
+        playervida = GameObject.FindWithTag("Player").GetComponent<VidaPlayer>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "PLayer")
+        {
+            currentDamageTime += Time.deltaTime;
+            if(currentDamageTime > damageTime)
+            {
+                playervida.vida += cantidad;
+                currentDamageTime = 0.0f;
+            }
+        }
+    }
+}
