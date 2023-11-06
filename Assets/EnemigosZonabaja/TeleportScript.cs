037c0d1db702f856003652e5d902b1c24a1b14d4
Pollogamerz <tbarajasglez05@gmail.com>
2023-11-06 16:00:47 -0600

    paraelbuild(todaviano)

---
+++ Assets/EnemigosZonabaja/TeleportScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TeleportScript : MonoBehaviour
+{
+    public string sceneToLoad; // Nombre de la escena a la que se teletransportará
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Verifica si el objeto que colisionó tiene la etiqueta "player"
+        if (other.CompareTag("Player"))
+        {
+            // Cargar la escena especificada en sceneToLoad
+            SceneManager.LoadScene("ZonaMedia");
+        }
+    }
+}
