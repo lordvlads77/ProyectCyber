76e3ef740a1ecd186b2ec2a67de1c74ae015b2dc
Pollogamerz <tbarajasglez05@gmail.com>
2023-11-06 03:32:35 -0600

    Asi se hace B)

--- Assets/EnemigosZonabaja/VidaPlayer.cs
+++ Assets/EnemigosZonabaja/VidaPlayer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class VidaPlayer : MonoBehaviour
 {
@@ -13,5 +14,10 @@ public class VidaPlayer : MonoBehaviour
     {
         vida = Mathf.Clamp(vida, 0, 100);
         barraDeVida.fillAmount = vida / 100;
+
+        if(vida <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
     }
 }
